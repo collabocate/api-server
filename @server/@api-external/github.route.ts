a542cc279d48b8e9a3fcfca71939338237785e9e
KBandipo~ <keneebandipo@gmail.com>
2025-03-28 15:26:09 +0100

    feat: add route to fetch issue templates with contents

--- @server/@api-external/github.route.ts
+++ @server/@api-external/github.route.ts
@@ -1,5 +1,5 @@
 import express, { IRouter } from 'express';
-import { createIssueController, getIssuesController, getPullRequestsController, getRepositoriesController,getIssueTemplatesController } from '@api-external/github.controller';
+import { createIssueController, getIssuesController, getPullRequestsController, getRepositoriesController,getIssueTemplatesController, getIssueTemplatesContentController } from '@api-external/github.controller';
 
 const router: IRouter = express.Router();
 
@@ -10,5 +10,5 @@ router.post('/issues', createIssueController);
 router.get('/issue-templates', getIssueTemplatesController);
 router.get('/pull-requests', getPullRequestsController);
 router.get('/repositories', getRepositoriesController);
-
+router.get('/templates/issues', getIssueTemplatesContentController);
 export { router };
