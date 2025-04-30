0e24401b57bdf050b664c3094f6c196eb8d6c64e
Daniel Oladepo <98762494+Danbaba1@users.noreply.github.com>
2025-04-30 07:41:54 -0400

    Update auth.controller.ts

--- @server/@api-auth/auth.controller.ts
+++ @server/@api-auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { success } from '@lib/helpers';
 import passport from 'passport';
 import { UserDocument } from '@server/@api-user/user.model';
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'; 
 import dotenv from 'dotenv';
 import { userInfo } from 'os';
 dotenv.config();
@@ -14,7 +14,7 @@ let response: { [key: string]: unknown } = {};
 
 //---------------------- AUTHENTICATION (SIGNUP AND LOGIN) -------------------------------//
 export const signupOrLoginWithGithubController = (req: Request, res: Response, next: NextFunction) => {
-  passport.authenticate("github", { session: false },
+  passport.authenticate("github", {session: false},
     (err: Error, user: UserDocument) => {
       try {
         if (err) {
@@ -23,9 +23,9 @@ export const signupOrLoginWithGithubController = (req: Request, res: Response, n
         }
 
         const token = jwt.sign(
-          { _id: user._id, email: user.email, role: user.role },
+          {_id: user._id, email: user.email, role: user.role},
           process.env.JWT_SECRET,
-          { expiresIn: process.env.JWT_LIFETIME }
+          {expiresIn: process.env.JWT_LIFETIME}
         );
 
         response = {
@@ -49,22 +49,22 @@ export const signupOrLoginWithGithubController = (req: Request, res: Response, n
       } catch (err) {
         next(err)
       }
-    })(req, res, next)
+    }) (req, res, next)
 }
 
 export const signupOrLoginWithGoogleController = (req: Request, res: Response, next: NextFunction) => {
-  passport.authenticate("google", { session: false },
+  passport.authenticate("google", {session: false},
     (err: Error, user: UserDocument) => {
       try {
         if (err) {
-          // const myErr = new Error("[UNAUTHORIZED] Invalid google user");
+        // const myErr = new Error("[UNAUTHORIZED] Invalid google user");
           throw err;
         }
 
         const token = jwt.sign(
-          { _id: user._id, email: user.email, role: user.role },
+          {_id: user._id, email: user.email, role: user.role},
           process.env.JWT_SECRET,
-          { expiresIn: process.env.JWT_LIFETIME }
+          {expiresIn: process.env.JWT_LIFETIME}
         );
 
         response = {
@@ -88,55 +88,55 @@ export const signupOrLoginWithGoogleController = (req: Request, res: Response, n
       } catch (err) {
         next(err)
       }
-    })(req, res, next)
+    }) (req, res, next)
 }
 
 export const signupWithLocalController = async (req: Request, res: Response, next: NextFunction) => {
-  passport.authenticate('local-signup', { session: false },
+  passport.authenticate('local-signup', {session: false}, 
     (err: Error, user: UserDocument) => {
-      try {
-        if (err) {
-          throw err;
-        }
-
-        const token = jwt.sign(
-          { _id: user._id, email: user.email, role: user.role },
-          process.env.JWT_SECRET,
-          { expiresIn: process.env.JWT_LIFETIME }
-        );
-
-        response = {
-          success: true,
-          data: {
-            user: {
-              _id: user._id,
-              email: user.email,
-              email_verified: user.email_verified,
-              role: user.role,
-              createdAt: user.createdAt,
-              updatedAt: user.updatedAt,
-            },
-            token: token
-          },
-          message: `SUCCESS: User local-signup was successfull`,
-        };
-        success(`SUCCESS: User local-signup was successfull`);
-        return res.status(201).json(response);
-
-      } catch (err) {
-        next(err);
+    try {
+      if (err) {
+        throw err;
       }
-    })(req, res, next);
+
+      const token = jwt.sign(
+        {_id: user._id, email: user.email, role: user.role},
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.JWT_LIFETIME}
+      );
+
+      response = {
+        success: true,
+        data: {
+          user: {
+            _id: user._id,
+            email: user.email,
+            email_verified: user.email_verified,
+            role: user.role,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
+          },
+          token: token
+        },
+        message: `SUCCESS: User local-signup was successfull`,
+      };
+      success(`SUCCESS: User local-signup was successfull`);
+      return res.status(201).json(response);
+
+    } catch (err) {
+      next(err);
+    }
+  })(req, res, next);
 }
 
 
 export const loginWithLocalController = async (req: Request, res: Response, next: NextFunction) => {
-  passport.authenticate('local-login', { session: false },
+  passport.authenticate('local-login', {session: false}, 
     (err: Error, user: UserDocument) => {
-      try {
-        if (err) {
-          throw err;
-        }
+    try {
+      if (err) {
+        throw err;
+      }
 
         const token = jwt.sign(
           { _id: user._id, email: user.email, username: user.username, role: user.role },
