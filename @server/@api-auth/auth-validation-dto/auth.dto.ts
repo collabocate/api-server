42c6902362b940b71e5e1b1e50874af42af1f456
samuko-things <samuel.c.agba@gmail.com>
2025-05-14 14:18:24 +0100

    added password validation

--- @server/@api-auth/auth-validation-dto/auth.dto.ts
+++ @server/@api-auth/auth-validation-dto/auth.dto.ts
@@ -1,5 +1,5 @@
 import { Expose } from 'class-transformer';
-import { IsString, IsNotEmpty, IsEmail} from 'class-validator';
+import { IsString, IsNotEmpty, IsEmail, MinLength, Matches} from 'class-validator';
 
 export class AuthDto {
   @Expose()
@@ -8,6 +8,8 @@ export class AuthDto {
   email: string;
 
   @Expose()
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).+$/, {message: 'password must be a mix of lowercase letter(s), uppercase letter(s), number(s) and symbol(s)'})
+  @MinLength(8)
   @IsString()
   @IsNotEmpty()
   password: string;
