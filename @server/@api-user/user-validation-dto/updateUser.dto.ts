1695dac12cdae78697145888dfb9942080a81a33
samuko-things <samuel.c.agba@gmail.com>
2025-05-16 14:56:40 +0100

    update password validation dto for users UPDATE endpoints

--- @server/@api-user/user-validation-dto/updateUser.dto.ts
+++ @server/@api-user/user-validation-dto/updateUser.dto.ts
@@ -1,8 +1,10 @@
 import { Expose } from 'class-transformer';
-import { IsString, IsNotEmpty} from 'class-validator';
+import { IsString, IsNotEmpty, Matches, MinLength} from 'class-validator';
 
 export class UpdateUserDto {
   @Expose()
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).+$/, {message: 'password must be a mix of lowercase letter(s), uppercase letter(s), number(s) and symbol(s)'})
+  @MinLength(8)
   @IsString()
   @IsNotEmpty()
   password: string;
