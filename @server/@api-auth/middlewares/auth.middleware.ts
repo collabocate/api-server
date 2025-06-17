7cd2f9e18d7535e586b8fd7c59a6bf78e1a178db
samuko-things <samuel.c.agba@gmail.com>
2025-06-17 13:34:13 +0100

    added redirect on failure for githun Oauth2

--- @server/@api-auth/middlewares/auth.middleware.ts
+++ @server/@api-auth/middlewares/auth.middleware.ts
@@ -60,7 +60,10 @@ export const authWithGoogle = (req: ReqUser, res: Response, next: NextFunction) 
 };
 
 export const authWithGithub = (req: ReqUser, res: Response, next: NextFunction) => {
-  passport.authenticate('github', {session: false},
+  passport.authenticate('github', {
+      session: false,
+      failureRedirect: `${process.env.APP_SUBDOMAIN_CLIENT_APP_URL}/login`
+    },
     () => {
     //nothing to do here for now
   }) (req, res, next)
